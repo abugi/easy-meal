a717158c4fa8df9d4c27b21dec73bca60e1c272a
abugi <abugiboi@gmail.com>
2019-06-30 23:17:28 +0100

    install body parser

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 9000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Server is live');
